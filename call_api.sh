e4c710d162b2e655db0da9cdad23314768bfb664
Luis Eduardo <eng.limaluis@gmail.com>
2025-10-19 07:32:47 -0300

    feat: scritp to call api (by uf and pag numbers)

---
+++ call_api.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# ./call_api.sh <estado> <tamanhoDaPagina>
+
+curl -X 'GET' \
+  "https://api.obrasgov.gestao.gov.br/obrasgov/api/projeto-investimento?uf=$1&pagina=0&tamanhoDaPagina=$2" \
+  -H 'accept: */*' | jq '.' >"data/$(date +%d_%H_%M).json"
