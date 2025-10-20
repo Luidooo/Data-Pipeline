de295ac75b7aff6105821e515a21c6289f41d8a5
Luis Eduardo Lima <eng.limaluis@gmail.com>
2025-10-20 16:09:46 -0300

    feat(jupnote): jup notebook

---
+++ notebooks/jupyter_config.py
@@ -0,0 +1,18 @@
+c.NotebookApp.open_browser = False
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.port = 8888
+c.NotebookApp.allow_root = True
+c.NotebookApp.token = ''
+c.NotebookApp.password = ''
+c.NotebookApp.notebook_dir = '/app/notebooks'
+c.NotebookApp.default_url = '/lab/tree/analise_obrasgov_df.ipynb'
+
+c.ServerApp.open_browser = False
+c.ServerApp.ip = '0.0.0.0'
+c.ServerApp.port = 8888
+c.ServerApp.allow_root = True
+c.ServerApp.token = ''
+c.ServerApp.password = ''
+c.ServerApp.root_dir = '/app/notebooks'
+
+c.LabApp.default_url = '/lab/tree/analise_obrasgov_df.ipynb'
