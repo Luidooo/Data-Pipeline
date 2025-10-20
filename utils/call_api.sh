c466fecc20e6a1b29984b60408eff93a59438c32
Luis Eduardo Lima <eng.limaluis@gmail.com>
2025-10-20 11:26:10 -0300

    wip(env): utils with randon shits

--- utils/call_api.sh
+++ utils/call_api.sh
@@ -4,4 +4,4 @@
 
 curl -X 'GET' \
   "https://api.obrasgov.gestao.gov.br/obrasgov/api/projeto-investimento?uf=$1&pagina=0&tamanhoDaPagina=$2" \
-  -H 'accept: */*' | jq '.' >"data/$(date +%d_%H_%M).json"
+  -H 'accept: */*' | jq '.' >"../data/$(date +%d_%H_%M).json"
